fd9d6b0a89da2084006e15aa1dc14055ee65fbbc
stepin104701 <69566858+stepin104701@users.noreply.github.com>
2020-09-18 23:30:36 +0530

    Create foodhead.h

---
+++ inc/foodhead.h
@@ -0,0 +1,25 @@
+#ifndef __foodhead_H__
+	#define __foodhead_H__
+
+void administration();
+void customer();
+void mainmenu();
+void addrecord(void);
+void deleterecord(void);
+void viewrecord(void);
+int getdata();
+int checkid();
+void vegetarian(void);
+void nonvegetarian(void);
+void returnfunc(void);
+
+
+
+int s;
+char findrecord;
+int again;
+int quantity;
+double total=0;
+int t;
+
+	#endif
